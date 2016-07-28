34c0fb049f87bb2be0d58a905a2147751a10110f
melissajimison <melissajimison@gmail.com>
2016-07-28 10:48:39 -0700

    created movies controller

--- app/models/movies.js
+++ app/models/movies.js
@@ -9,17 +9,3 @@ export default DS.Model.extend({
   createdAt: DS.attr('date'),
   updatedAt: DS.attr('date')
 });
-//
-//
-// import Model from 'ember-data/model';
-// import attr from 'ember-data/attr';
-//
-// export default Model.extend({
-//   title: attr('string'),
-//   release_date: attr('string'),
-//   overview: attr('string'),
-//   inventory: attr('string'),
-//   poster_filename: attr('string'),
-//   createdAt: attr('date'),
-//   updatedAt: attr('date')
-// });
