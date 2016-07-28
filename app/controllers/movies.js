ae831b442da279b4d60e57da995cf74047bbd7c9
melissajimison <melissajimison@gmail.com>
2016-07-28 11:04:17 -0700

    made the fuctionality to show and hide info by clicking

--- app/controllers/movies.js
+++ app/controllers/movies.js
@@ -1,4 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-});
+  isExpanded: false,
+
+    actions: {
+      showInfo: function() {
+        this.set('isExpanded', true);
+      },
+
+      hideInfo: function() {
+        this.set('isExpanded', false);
+      }
+    }
+  });
